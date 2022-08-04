4c4cfbc2ffaf922951d7bdb8c689752b5a23e8c8
Antonov-S <anto1nov1@gmail.com>
2022-08-04 17:53:10 +0300

    Add useReducer and create StateContext and DispatchContext

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -3,20 +3,20 @@ import { useNavigate } from "react-router-dom";
 import Axios from "axios";
 
 import Page from "./Page";
-import ExampleContext from "../ExampleContext";
+import DispatchContext from "../DispatchContext";
 
 function CreatePost() {
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
   const navigate = useNavigate();
-  const { addFlashMessage } = useContext(ExampleContext);
+  const appDispatch = useContext(DispatchContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
       const response = await Axios.post("/create-post", { title, body, token: localStorage.getItem("complexappToken") });
       // Redirect to new post url
-      addFlashMessage("Congrats, you successfully created a post!");
+      appDispatch({ type: "flashMessage", value: "Congrats, you created a new post." });
       navigate(`/post/${response.data}`);
       console.log("New post was created!");
     } catch (e) {
