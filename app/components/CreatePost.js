8e619d855b8d34ed07a05663a3fddd37db568553
Antonov-S <anto1nov1@gmail.com>
2022-08-05 01:04:36 +0300

    Add user property in initialState and pull his data from this object  instead of localStorage

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -4,17 +4,19 @@ import Axios from "axios";
 
 import Page from "./Page";
 import DispatchContext from "../DispatchContext";
+import StateContext from "../StateContext";
 
 function CreatePost() {
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
   const navigate = useNavigate();
   const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      const response = await Axios.post("/create-post", { title, body, token: localStorage.getItem("complexappToken") });
+      const response = await Axios.post("/create-post", { title, body, token: appState.user.token });
       // Redirect to new post url
       appDispatch({ type: "flashMessage", value: "Congrats, you created a new post." });
       navigate(`/post/${response.data}`);
