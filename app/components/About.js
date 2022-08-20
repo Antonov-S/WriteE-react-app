6435c69669ec9f8a49c39ccd43be788760ecbd04
Antonov-S <anto1nov1@gmail.com>
2022-08-20 19:42:43 +0300

    Adding some text in About

--- app/components/About.js
+++ app/components/About.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import Page from "./Page";
 
 function About() {
   return (
     <Page title="About Us">
-      <h2>About Us!!!!!!</h2>
-      <p className="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolorum labore quisquam vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid error veritatis consequuntur, excepturi cumque fuga eum incidunt doloremque?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At qui enim rem totam voluptatum. Aut saepe temporibus, facilis ex a iste expedita minima dolorum dicta doloribus libero aliquid, quae maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit beatae eum, est soluta ducimus ratione et impedit sapiente, nihil, atque dignissimos adipisci? Totam atque officia quis voluptates sed veniam?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quisquam possimus tenetur, dicta enim rerum quis, quaerat id nobis provident quo dolorum sapiente temporibus facere non repellendus consequatur cupiditate!</p>
+      <h2>About Us</h2>
+      <p className="lead text-muted">WriteE is what’s happening and what people are talking about right now.</p>
+      <p>Are you sick of short tweets and impersonal “shared” posts that are reminiscent of the late 90’s email forwards? We believe getting back to actually writing is the key to enjoying the internet again. We serve the public conversation. That’s why it matters to us that people have a free and safe space to talk.</p>
+      <p>WriteE is an open service that's home to a world of diverse people, perspectives, ideas, and information. Life’s not about a job, it’s about purpose. We believe real change starts with conversation. Here, your voice matters. Come as you are and together we’ll do what’s right (not what’s easy) to serve the public conversation.</p>
+      <p>
+        Go to <Link to={"/"}>WriteE</Link> to sign up for free account or login in to catch up.
+      </p>
     </Page>
   );
 }
