dab813a15ef0d4810cb61087529e59b95c0a9f14
Antonov-S <anto1nov1@gmail.com>
2022-08-20 19:09:07 +0300

    Adding real text for Terms and Conditions

--- README.md
+++ README.md
@@ -12,17 +12,16 @@
 
 This is **fullstack app**, focused mainly on the frontend with modern React, therefore some of the other elements in it are pretty basic - the backend is a dummy api created for educational purposes, used html and css are from free social-network theme, etc...
 
-**Note:** The latest stable version of the application can be found at the following [address](https://capable-crumble-2a2ca0.netlify.app). **The backend is hosted on free thier Heroku service**, so if the app doesn't receive any traffic in 1 hour goes to sleep and when someone accesses it takes time to wake up. This causes a short delay for this first request, but subsequent requests will perform normally.
+**Note:** The latest stable version of the application can be found at the following [address](https://capable-crumble-2a2ca0.netlify.app). **The backend is hosted on free thier Heroku service**, so if the app doesn't receive any traffic in one hour goes to sleep and this causes a short delay for this first request, but subsequent requests will perform normally.
 
 ## Technologies
 
 - [react](https://www.npmjs.com/package/react)
-- [react-router-dom](https://www.npmjs.com/package/react-router-dom)
 - [webpack](https://www.npmjs.com/package/webpack)
 - [axios](https://www.npmjs.com/package/axios)
 - [use-immer](https://www.npmjs.com/package/use-immer)
 - [react-transition-group](https://www.npmjs.com/package/react-transition-group)
-- [socket.io-client](https://www.npmjs.com/package/socket.io-client)
+- [socket.io-client](https://socket.io/docs/v4/client-initialization/)
 - [react-tooltip](https://www.npmjs.com/package/react-tooltip)
 - [react-markdown](https://www.npmjs.com/package/react-markdown)
 
@@ -30,4 +29,4 @@ This is **fullstack app**, focused mainly on the frontend with modern React, the
 
 ## Further
 
-You may create [file an issue](https://github.com/Antonov-S/WriteE-react-app), regarding missing, incorrect or incomplete information, regarding the use of this service. Any contribution is welcome!
+You may create [file an issue](https://github.com/Antonov-S/WriteE-react-app/issues/new), regarding missing, incorrect or incomplete information, regarding the use of this service. Any contribution is welcome!
--- app/components/Terms.js
+++ app/components/Terms.js
@@ -5,11 +5,13 @@ function Terms() {
   return (
     <Page title="Terms and Conditions">
       <h2>Our Terms &amp; Conditions</h2>
-      <p className="lead text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis dolorum labore quisquam vel id dicta fuga! Ducimus, quo. Dolore commodi aliquid error veritatis consequuntur, excepturi cumque fuga eum incidunt doloremque?</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At qui enim rem totam voluptatum. Aut saepe temporibus, facilis ex a iste expedita minima dolorum dicta doloribus libero aliquid, quae maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat suscipit beatae eum, est soluta ducimus ratione et impedit sapiente, nihil, atque dignissimos adipisci? Totam atque officia quis voluptates sed veniam?</p>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quisquam possimus tenetur, dicta enim rerum quis, quaerat id nobis provident quo dolorum sapiente temporibus facere non repellendus consequatur cupiditate!</p>
-      <h3>Details</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae laboriosam distinctio atque sint earum? Temporibus, voluptas aspernatur aliquam nisi sed harum laborum, nemo odio animi officia quisquam. Veniam, natus reprehenderit.</p>
+      <p className="lead text-muted">You may use the Services only if you agree to form a binding contract with WriteE and are not a person barred from receiving services under the laws of the applicable jurisdiction. In any case, you must be at least 13 years old, or in the case of Periscope 16 years old, to use the Services. If you are accepting these Terms and using the Services on behalf of a company, organization, government, or other legal entity, you represent and warrant that you are authorized to do so and have the authority to bind such entity to these Terms, in which case the words “you” and “your” as used in these Terms shall refer to such entity.</p>
+      <p>You retain your rights to any Content you submit, post or display on or through the Services. What’s yours is yours — you own your Content (and your incorporated audio, photos and videos are considered part of the Content).</p>
+      <p>You represent and warrant that you have, or have obtained, all rights, licenses, consents, permissions, power and/or authority necessary to grant the rights granted herein for any Content that you submit, post or display on or through the Services. You agree that such Content will not contain material subject to copyright or other proprietary rights, unless you have necessary permission or are otherwise legally entitled to post the material and to grant WriteE the license described above.</p>
+      <h3>Before you scroll, read this... Privacy Policy</h3>
+      <p>It’s really hard to make everyone happy with a Privacy Policy. Most people who use WriteE want something short and easy to understand. While we wish we could fit everything you need to know into a Post, our regulators ask us to meet our legal obligations by describing them all in a lot of detail.</p>
+      <p>With that in mind, we’ve written our Privacy Policy as simply as possible to empower you to make informed decisions when you use WriteE by making sure you understand and have control over the information we collect and how it’s used.</p>
+      <p>So if you skip reading every word of the Privacy Policy, at least know this: WriteE is a public platform and We collect some data about you</p>
     </Page>
   );
 }
